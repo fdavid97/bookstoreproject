9201302d808126ef8151e5af46c19e7a98b6ba23
fdavid97 <farkas.david613@gmail.com>
2023-09-14 22:02:28 +0200

    First commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://farkas_david613:engedjbe@cluster0.0vwybqp.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+    </Routes>
+  )
+}
+
+export default App
